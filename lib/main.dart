b6ec9bdd283b36eb8090165b8445efe80195dce7
bahri <bahri@atmatech.net>
2022-01-10 08:10:52 +0700

    Update widget data with stateless incorreclty

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer chosen!');
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
   }
 
   @override
@@ -20,7 +23,9 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              Text('The question!'),
+              Text(
+                questions[questionIndex],
+              ),
               RaisedButton(
                 onPressed: answerQuestion,
                 child: Text('Answer 1'),
